49ce10ec9a563d656d823d3393488f93b0e12fdc
AntAndTim <anton.timchenko@outlook.com>
2019-09-06 11:57:11 +0300

    Get token is lazy now

--- src/main/java/EntryPoint.java
+++ src/main/java/EntryPoint.java
@@ -1,17 +1,16 @@
-import token.Token;
-
+import java.io.EOFException;
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class EntryPoint {
+
     public static void main(String[] args) throws IOException {
         Lexer lexer = new Lexer(EntryPoint.class.getClassLoader().getResourceAsStream("testFile.js"));
-        ArrayList<Token> result = lexer.getToken();
-        while (result != null ) {
-            for (Token aResult : result) {
-                System.out.println(aResult);
+        while (true) {
+            try {
+                System.out.println(lexer.getToken());
+            } catch (EOFException e) {
+                break;
             }
-            result = lexer.getToken();
         }
     }
 }
--- src/main/java/Lexer.java
+++ src/main/java/Lexer.java
@@ -1,70 +1,68 @@
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import token.Token;
 import token.TokenType;
+import token.type.Delimiter;
 import token.type.Identifier;
 import token.type.Keyword;
 import token.type.Operator;
-import token.type.Delimiter;
-
-import static token.type.Delimiter.NEWLINE;
-import static token.type.Delimiter.WHITESPACE;
 
 public class Lexer {
 
-    private static final Map<String, TokenType> DELIMITER_ELEMENTS = Stream
-            .of(Delimiter.values())
-            .collect(Collectors.toMap(anEnum -> anEnum.getValue(), anEnum -> anEnum));
-
     private static final Map<String, TokenType> LANGUAGE_ELEMENTS = Stream
-        .of(Keyword.values(), Operator.values())
+        .of(Keyword.values(), Operator.values(), Delimiter.values())
         .flatMap(Arrays::stream)
         .collect(Collectors.toMap(anEnum -> anEnum.getValue(), anEnum -> anEnum));
-
+    private static final char EOF = (char) -1;
 
     private final InputStream jsFileStream;
     private int currentPosition = 1;
     private int currentLine = 1;
     private boolean startFromNextLine;
-    private boolean nextCharIsLast = false;
+    private Optional<Token> nextToken = Optional.empty();
 
     public Lexer(InputStream jsFileStream) {
         this.jsFileStream = jsFileStream;
     }
 
-    ArrayList<Token> getToken() throws IOException {
+    Token getToken() throws IOException {
+        if (nextToken.isPresent()) {
+            Token token = nextToken.get();
+            nextToken = Optional.empty();
+            return token;
+        }
         char symbol = readSymbol();
-        String value = symbol == '\n' ? "" : getValue(symbol);
-        ArrayList<Token> result = new ArrayList<Token>();
+        String value = getValue(symbol);
         while (!checkValue(value)) {
-            if (DELIMITER_ELEMENTS.get(String.valueOf(symbol)) != null && !value.equals("")) {
-                if (DELIMITER_ELEMENTS.get(String.valueOf(symbol)) == WHITESPACE) {
-                    result.add(new Token(Identifier.IDENTIFIER, value, currentPosition - value.length() - 1, currentLine));
-                }
-                else {
-                    result.add(new Token(Identifier.IDENTIFIER, value.substring(0, value.length() - 1), currentPosition - value.length() - 1, currentLine));
-                }
-                result.add(new Token(DELIMITER_ELEMENTS.get(String.valueOf(symbol)), String.valueOf(symbol), currentPosition - value.length() - 1, currentLine));
-                return result;
-            }
-            else if (value.equals("")) {
-                result.add(new Token(symbol == '\n' ? NEWLINE : WHITESPACE, symbol == '\n'? "\\n":" ",currentPosition - value.length() - 1, currentLine));
-                return result;
-            }
-
             symbol = readSymbol();
-            value += symbol == '\n' ? "" : getValue(symbol);
-            if (symbol == Character.MIN_VALUE) {
-                return null;
+            String newSymbolValue = getValue(symbol);
+            if (checkValue(newSymbolValue) && !value.equals("")) {
+                nextToken = Optional.of(getElement(newSymbolValue));
+                return getIdentifier(value);
+            }
+            value += newSymbolValue;
+            if (symbol == EOF) {
+                throw new EOFException("All the possible tokens already obtained");
             }
         }
-        result.add(new Token(LANGUAGE_ELEMENTS.get(value), value, currentPosition - value.length(), currentLine));
-        return result;
+        return getElement(value);
+    }
+
+    private Token getIdentifier(String value) {
+        return new Token(Identifier.IDENTIFIER, value, currentPosition - value.length() - 1, currentLine);
+    }
+
+    private Token getElement(String value) {
+        return new Token(LANGUAGE_ELEMENTS.get(value),
+                         value.equals("\n") ? "\\n" : value,
+                         currentPosition - value.length(),
+                         currentLine);
     }
 
     private String getValue(char symbol) {
@@ -76,9 +74,6 @@ public class Lexer {
     }
 
     private char readSymbol() throws IOException {
-        if (nextCharIsLast) {
-            return Character.MIN_VALUE;
-        }
         if (startFromNextLine) {
             currentLine++;
             currentPosition = 1;
@@ -87,12 +82,8 @@ public class Lexer {
         currentPosition++;
 
         char nextChar = (char) jsFileStream.read();
-        if (jsFileStream.available() == 0) {
-            nextCharIsLast = true;
-        }
         if (nextChar == '\n') {
             startFromNextLine = true;
-            return '\n';
         }
         return nextChar;
     }
